e377b0c9afc118f7a106a82b08600ae788a46d72
iTubePod <hmgc_mauro@icloud.com>
2016-06-06 10:05:28 -0500

    Back-Up

--- HWs/First Project/FirstProject.cpp
+++ HWs/First Project/FirstProject.cpp
@@ -3,15 +3,16 @@
 #include <sstream>
 #include <vector>
 
+bool Reflexive (std::vector<int> pairs, std::vector<int> members);
+bool Transitive (std::vector<int> pairs, std::vector<int> members);
+bool Symmetric (std::vector<int> pairs, std::vector<int> members);
+
 int main ()
 {
 	std::ifstream input;
-	int tmp, ltmp, fpair, spair;
+	int tmp;
 	int x;
 	bool unique=true;
-	bool Reflexive = true;
-	bool Transitive = true;
-	bool Symmetric = true;
 	std::vector <int> pairs;
 	std::vector <int> members;
 
@@ -22,7 +23,6 @@ int main ()
 		std::cout<<"Fatal Error, unable to open file"<<std::endl;
 	}
 	input >> x;
-	std::cout<<x<<std::endl;
 	//Reads all the pairs from the file
 	for (int i=1;i<=x;i++)
 	{
@@ -32,11 +32,6 @@ int main ()
 			pairs.push_back(tmp);
 		}
 	}
-	/*for (int j=0;j<pairs.size();)
-	{
-		std::cout<<pairs[j]<<" "<<pairs[j++]<<std::endl;
-		j++;
-	}*/
 	//detects all the members of the relation
 	for (int j=0;j<pairs.size();j++)
 	{
@@ -63,7 +58,17 @@ int main ()
 	}
 	std::cout<<"}"<<std::endl;
 
+	//prints the properties that the relation meets
+	std::cout << "La relacion es: " <<( (Reflexive(pairs, members)) ? "REFLEXIVA " : "IRREFLEXIVA ") <<( (Transitive(pairs, members)) ? "TRANSITIVA " : "")<<( (Symmetric(pairs, members)) ? "SIMETRICA " : "ASIMETRICA ")<< std::endl;
+	if(!Reflexive && !Transitive && !Symmetric)
+		std::cout<< "“NO CUMPLE NINGUNA" <<std::endl;
+
+}
+bool Reflexive (std::vector<int> pairs, std::vector<int> members)
+{
 	//Reflexive test
+	bool Reflexive = true;
+	int tmp, ltmp;
 	for (int i=0; i<members.size();i++)
 	{
 		tmp=members[i];
@@ -81,28 +86,30 @@ int main ()
 			j++;
 		}
 	}
-	if (Reflexive)
-		std::cout<<"REFLEXIVE"<<std::endl;
-	else
-		std::cout<<"IRREFLEXIVE"<<std::endl;
+	return Reflexive;
+}
+bool Transitive (std::vector<int> pairs, std::vector<int> members)
+{
 	//Transitive test
+	bool Transitive = true;
+	int fpair, spair;
 	for (int i=0; i<pairs.size()-1;i++)
 	{
 		fpair=pairs[i];
 		spair=pairs[i+1];
-		std::cout<<"Pair "<<fpair<<", "<<spair<<std::endl;
+		//std::cout<<"Pair "<<fpair<<", "<<spair<<std::endl;
 		for (int j=0; j<pairs.size()-1;j++)
 		{
 			if(spair==pairs[j])
 			{
 				spair=pairs[j+1];
-				std::cout<<"Looking for pair "<<fpair<<", "<<spair<<std::endl;
+				//std::cout<<"Looking for pair "<<fpair<<", "<<spair<<std::endl;
 				for (int k=0;k<pairs.size(); k++)
 				{
 					if(fpair==pairs[k] && spair==pairs[k+1])
 					{
 						Transitive=Transitive*true;
-						std::cout<<"FOUND!"<<std::endl;
+						//std::cout<<"FOUND!"<<std::endl; 
 						j=pairs.size();
 						i++;
 						i++;
@@ -112,7 +119,7 @@ int main ()
 						if (k==(pairs.size()-2))
 						{
 							Transitive=Transitive*false;
-							std::cout<<"NOT FOUND!"<<std::endl;
+							//std::cout<<"NOT FOUND!"<<std::endl;
 						}
 					}
 					k++;
@@ -122,22 +129,24 @@ int main ()
 		}
 		i++;
 	}
-	if (Transitive)
-		std::cout<<"Transitive"<<std::endl;
-	else
-		std::cout<<"Not Transitive"<<std::endl;
+	return Transitive;
+}
+bool Symmetric (std::vector<int> pairs, std::vector<int> members)
+{
 	//Symmetric test
+	bool Symmetric = true;
+	int fpair, spair;
 	for (int i=0;i<pairs.size();i++)
 	{
 		fpair=pairs[i];
 		spair=pairs[i+1];
-		std::cout<<"Looking for pair "<<spair<<", "<<fpair<<std::endl;
+		//std::cout<<"Looking for pair "<<spair<<", "<<fpair<<std::endl;
 		for (int j = 0; j < pairs.size(); j++)
 		{
 			if(spair==pairs[j] && fpair==pairs[j+1])
 			{
 				Symmetric=Symmetric*true;
-				std::cout<<"FOUND!"<<std::endl;
+				//std::cout<<"FOUND!"<<std::endl;
 				j=pairs.size();
 			}
 			else
@@ -145,16 +154,12 @@ int main ()
 				if(j==(pairs.size()-2))
 				{
 					Symmetric=Symmetric*false;
-					std::cout<<"NOT FOUND!"<<std::endl;
+					//std::cout<<"NOT FOUND!"<<std::endl;
 				}
 			}
 			j++;
 		}
 		i++;		
 	}
-	if (Symmetric)
-		std::cout<<"Symmetric"<<std::endl;
-	else
-		std::cout<<"Not Symmetric"<<std::endl;
-
+	return Symmetric;
 }
