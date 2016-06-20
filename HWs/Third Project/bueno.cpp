d1557d3749fc76154c39e6f3c78545824ea10fd4
iTubePod <hmgc_mauro@icloud.com>
2016-06-20 17:03:40 -0500

    Back-Up

--- HWs/Third Project/bueno.cpp
+++ HWs/Third Project/bueno.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
 #include <vector>
-void combine (std::vector<std::string> *comb)
+#include <algorithm>
+std::vector<std::vector<std::string>> combination (std::vector<std::string> states)
 {
-
+	//std::cout<<states.size();
+	std::vector<std::string> tmp;
+	std::vector<std::vector<std::string>> rtrn;
+	for (int i = 0; i < states.size(); i++)
+	{
+		//std::cout<<std::endl<<i;
+		for (int j = 0; j < states.size(); j++)
+		{
+			//std::cout<<std::endl<<j;
+			if (i==0 && j==0)
+				tmp.push_back("/");
+			else if (i==0 && j==1)
+				tmp.push_back(states[0]);
+			else if (i==1 && j==0)
+				tmp.push_back(states[1]);
+			else if (i==1 && j==1)
+			{
+				tmp.push_back(states[0]);
+				tmp.push_back(states[1]);
+			}
+			rtrn.push_back(tmp);
+			tmp.clear();
+		}
+	}
+	return rtrn;
 }
+
 int main ()
 {
 	//infile
@@ -15,7 +40,8 @@ int main ()
 		char a, TN;
 		std::string NFA, tmp, IS;
 		std::vector <int> n;
-		std::vector <std::string> states, FS;
+		std::vector <std::string> states, FS, nFS;
+		std::vector<std::vector<std::string>> nStates;
 		input.open("ex.txt");
 		if (!input.fail())
 		{
@@ -55,7 +81,29 @@ int main ()
 					input>>table[i][j];
 				}
 			}
-		}
+			nStates = combination (states);
+			for (int i = 0; i < nStates.size(); i++)
+			{
+				for (int j = 0; j < nStates[i].size(); j++)
+				{
+					std::cout<<nStates[i][j]<<" ";
+				}
+				std::cout<<" | ";
+			}
+			std::string ntable [n.size()+1] [nStates.size()+1];
+			for (int i = 0; i <= n.size(); i++)
+			{
+				for (int j = 0; j <= nStates.size(); j++)
+				{
+					if(i==0 &&)
+						ntable[i][j]="/";
+					else
+					{
+						if(nStates[j].size==1)
+							ntable[i][j]=table[i][j];
+					}
+				}
+			}
 		else
 		{
 			std::cout<<"Fatal Error, unable to open file"<<std::endl;
