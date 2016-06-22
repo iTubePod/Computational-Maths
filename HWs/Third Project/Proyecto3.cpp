c977bb3d45fb6e6f4fabc2f1bbde4455424549a4
iTubePod <hmgc_mauro@icloud.com>
2016-06-21 22:01:35 -0500

    Back-Up

--- HWs/Third Project/Proyecto3.cpp
+++ HWs/Third Project/Proyecto3.cpp
@@ -4,13 +4,18 @@
 #include <vector>
 #include <algorithm>
 #include <sstream>
+#include <iomanip>
 
 std::vector<std::vector<std::string> > combination (std::vector<std::string> states);
 std::vector<std::vector<std::string> > newTable(std::vector<std::vector<std::string> > table, std::vector<std::vector<std::string> > nStates);
-std::vector<std::vector<std::string> > FillnewTable(std::vector<std::vector<std::string> > table, std::vector<std::vector<std::string> > ntable);
+std::vector<std::vector<std::string> > FillnewTable(std::vector<std::vector<std::string> > table, std::vector<std::vector<std::string> > ntable, std::vector <int> n);
 std::string findSt(int y, std::string tmp, std::vector<std::vector<std::string>> table);
 void insert (std::string ins, std::string &where, bool dash);
 int findPos(std::string tmp, std::vector<std::vector<std::string>> table);
+void cleanup(std::vector<std::vector<std::string> > &final);
+void printT(std::vector<int> n, std::vector<std::vector<std::string> >ntable);
+void printQ(std::vector<std::vector<std::string> >ntable);
+void printF(std::vector<std::vector<std::string> >ntable, std::vector<std::string> final);
 
 int main ()
 {
@@ -20,7 +25,7 @@ int main ()
 		char a, TN;
 		std::string NFA, tmp, IS, fs;
 		std::vector <int> n;
-		std::vector <std::string> states, FS, nFS;
+		std::vector <std::string> states, FS;
 		std::vector<std::vector<std::string> > table, ntable, nStates;
 		input.open("ex.txt");
 		if (!input.fail())
@@ -66,25 +71,11 @@ int main ()
 			}		
 			nStates = combination (states);
 			ntable = newTable(table, nStates);
-			/*for (int i = 0; i < table.size(); i++)
-			{
-				for (int j = 0; j < table[i].size(); j++)
-				{
-					std::cout<<table[i][j]<<" ";
-				}
-				std::cout<<std::endl;
-			}*/
-			ntable=FillnewTable(table, ntable);
-			for (int i = 0; i < ntable.size(); i++)
-			{
-				for (int j = 0; j < ntable[i].size(); j++)
-				{
-					std::cout<<ntable[i][j]<<" | ";
-					if(j==0)
-						std::cout<<"||";
-				}
-				std::cout<<std::endl;
-			}
+			ntable=FillnewTable(table, ntable, n);
+			cleanup(ntable);
+			printQ(ntable);
+			printF(ntable, FS);
+			printT(n, ntable);
 		}
 		else
 		{
@@ -165,24 +156,32 @@ int findPos(std::string tmp, std::vector<std::vector<std::string>> table)
 		}
 	}
 }
-std::vector<std::vector<std::string> > FillnewTable(std::vector<std::vector<std::string> > table, std::vector<std::vector<std::string> > ntable)
+std::vector<std::vector<std::string> > FillnewTable(std::vector<std::vector<std::string> > table, std::vector<std::vector<std::string> > ntable, std::vector <int> n)
 {
 	std::string tmp, var, fn;
 	int x;
+	for (int i = 0; i < 1; i++)
+	{
+		for (int j = 0; j < n.size(); j++)
+		{
+				ntable[i].push_back("/");
+		}
+	}
 	for (int i = 1; i < ntable.size(); i++)
 	{
 		for (int j = 0; j < table.size(); j++)
 		{
-			//std::cout<<ntable[i][0]<<"<ntable "<<table[j][0]<<std::endl;
-			for (int k = 1; k < table[j].size(); k++)
+			if(j==0)
 			{
-				ntable[i].push_back("");
+				for (int k = 1; k <= n.size(); k++)
+				{
+					ntable[i].push_back("");
+				}
 			}
 			if(ntable[i][0].find("-"))
 			{
 				var=ntable[i][0];
 				std::istringstream ss(var);
-				ntable[i].push_back("");
 				while(std::getline(ss, tmp, '-')) 
 				{
 					x=findPos(tmp, table);
@@ -192,7 +191,9 @@ std::vector<std::vector<std::string> > FillnewTable(std::vector<std::vector<std:
 							insert(table[x][k], ntable[i][k], true);
 						else
 							insert(table[x][k], ntable[i][k], false);
+						std::cout<<"First k loop: "<<table[x][k]<<" in space "<<i<<" "<<k<<" ";
 					}
+					std::cout<<std::endl;
 				}
 			}
 			else
@@ -202,7 +203,9 @@ std::vector<std::vector<std::string> > FillnewTable(std::vector<std::vector<std:
 					for (int k = 1; j < table[j].size(); k++)
 					{
 						insert(table[j][k], ntable[i][k], false);
+						std::cout<<"Second k loop: "<<table[x][k]<<" in space "<<i<<" "<<k<<" ";
 					}
+					std::cout<<std::endl;
 				}
 			}
 		}
@@ -211,7 +214,79 @@ std::vector<std::vector<std::string> > FillnewTable(std::vector<std::vector<std:
 }
 void insert (std::string ins, std::string &where, bool dash)
 {
-	where=where+ins;
-	if(dash)
-		where=where+"-";
+	if(ins=="/"&&where!="")
+	{
+		where=where;
+	}
+	else
+	{
+		if(where.find(ins)==std::string::npos)
+		{
+			if(ins.find(where)!=std::string::npos)
+				where="";
+			if(dash&&where!="")
+				where=" "+where;
+			where=where+ins;
+		}
+	}
+}
+void cleanup(std::vector<std::vector<std::string> > &final)
+{
+	for (int i = 0; i < final.size(); ++i)
+	{
+		for (int j = 0; j < final[i].size(); j++)
+		{
+			std::replace( final[i][j].begin(), final[i][j].end(), '-', ' ');
+			std::replace( final[i][j].begin(), final[i][j].end(), '/', 'n');
+		}
+	}
+}
+void printT(std::vector<int> n, std::vector<std::vector<std::string> >ntable)
+{
+	std::cout<< '|' << std::setw(10) << "S'"<<'|';
+	for (int i = 0; i < n.size(); i++)
+	{
+		std::cout << std::setw(10) << n[i]<<'|';
+	}
+	std::cout<<std::endl;
+	for (int i = 0; i < ntable.size(); i++)
+	{
+		for (int j = 0; j < ntable[i].size(); j++)
+		{
+			std::cout << '|' << std::setw(10) << ntable[i][j];
+		}
+		std::cout<< '|' << std::endl;
+	}
+}
+void printQ(std::vector<std::vector<std::string> >ntable)
+{
+	std::cout<<"Q' = { ";
+	for (int i = 0; i < ntable.size(); i++)
+	{
+		if(i==ntable.size()-1)
+			std::cout<<ntable[i][0]<<" }"<<std::endl;
+		else
+			std::cout<<ntable[i][0]<<", ";
+	}
+}
+void printF(std::vector<std::vector<std::string> >ntable, std::vector<std::string> final)
+{
+	std::cout<<"F' = { ";
+	for (int i = 0; i < ntable.size(); i++)
+	{
+		for (int j = 1; j < ntable[i].size(); j++)
+		{
+			for (int k = 0; k < final.size(); k++)
+			{
+				if(ntable[i][j].find(final[k])==std::string::npos)
+				{
+					if(ntable[i][0]!="n")
+						std::cout<<ntable[i][0];
+					k=final.size();
+					j=ntable[i].size();
+				}
+			}
+		}
+	}
+	std::cout<<" }"<<std::endl;
 }
